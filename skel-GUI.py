18b944d322118b241d98d639eb4371e9b54e21ed
yushiiii <41015100+yushiiii@users.noreply.github.com>
2019-04-13 01:43:10 +0530

    Update skel-GUI.py

--- skel-GUI.py
+++ skel-GUI.py
@@ -51,19 +51,130 @@ class PageOne(Frame):
 		controller.show_frame(PageTwo)
 
 class PageTwo(Frame):
+    
+    expression = ""
+	
 
-	def __init__(self, parent, controller):
+    def press(num): 
+	     
+	    global expression 
+	
+	    expression = expression + str(num) 
+ 
+	    equation.set(expression) 
+ 
+    def equalpress(): 
+	 
+	    try: 
+            
+             global expression 
+	        
+             total = str(eval(expression)) 
+	        
+             equation.set(total) 
+            
+             expression = "" 
+            
+            except: 
+            
+             equation.set(" error ") 
+             expression = "" 
 
-		Frame.__init__(self, parent)
-		self.controller=controller
-		Label(self, text="Next Page").pack()
-		Button(self, text="Next", command=lambda:self.closeCur(controller)).pack()
+    def __init__(self,parent,controller):
+        Frame.__init__(self,parent)
+        self.controller=controller
+        Label(self, text="Calculator").pack()
+        Button(self, text="Next", command=lambda:self.closeCur(controller)).pack() 
+
+        # Driver code 
+        if __name__ == "__main__": 
+            
+            gui = Tk()  
+        
+            equation = StringVar() 
+
+            expression_field = Entry(gui, textvariable=equation) 
+
+            expression_field.grid(columnspan=4, ipadx=150) 
+
+            equation.set('enter your expression') 
+
+            button1 = Button(gui, text=' 1 ', fg='black', 
+                            command=lambda: press(1), height=5, width=11) 
+            button1.grid(row=2, column=0) 
+
+            button2 = Button(gui, text=' 2 ', fg='black', 
+                            command=lambda: press(2), height=5, width=11) 
+            button2.grid(row=2, column=1) 
+
+            button3 = Button(gui, text=' 3 ', fg='black', 
+                            command=lambda: press(3), height=5, width=11) 
+            button3.grid(row=2, column=2) 
+
+            button4 = Button(gui, text=' 4 ', fg='black', 
+                            command=lambda: press(4), height=5, width=11) 
+            button4.grid(row=3, column=0) 
+
+            button5 = Button(gui, text=' 5 ', fg='black', 
+                            command=lambda: press(5), height=5, width=11) 
+            button5.grid(row=3, column=1) 
+
+            button6 = Button(gui, text=' 6 ', fg='black', 
+                            command=lambda: press(6), height=5, width=11) 
+            button6.grid(row=3, column=2) 
+
+            button7 = Button(gui, text=' 7 ', fg='black', 
+                            command=lambda: press(7), height=5, width=11) 
+            button7.grid(row=4, column=0) 
+
+            button8 = Button(gui, text=' 8 ', fg='black', 
+                            command=lambda: press(8), height=5, width=11) 
+            button8.grid(row=4, column=1) 
+
+            button9 = Button(gui, text=' 9 ', fg='black', 
+                            command=lambda: press(9), height=5, width=11) 
+            button9.grid(row=4, column=2) 
+
+            button0 = Button(gui, text=' 0 ', fg='black', 
+                            command=lambda: press(0), height=5, width=11) 
+            button0.grid(row=5, column=0) 
+
+            plus = Button(gui, text=' + ', fg='black', 
+                        command=lambda: press("+"), height=5, width=11) 
+            plus.grid(row=2, column=3) 
+
+            minus = Button(gui, text=' - ', fg='black', 
+                        command=lambda: press("-"), height=5, width=11) 
+            minus.grid(row=3, column=3) 
+
+            multiply = Button(gui, text=' * ', fg='black', 
+                            command=lambda: press("*"), height=5, width=11) 
+            multiply.grid(row=4, column=3) 
+
+            divide = Button(gui, text=' / ', fg='black', 
+                            command=lambda: press("/"), height=5, width=11) 
+            divide.grid(row=5, column=3) 
+
+            equal = Button(gui, text=' = ', fg='black', 
+                        command=equalpress, height=5, width=11) 
+            equal.grid(row=5, column=2) 
+
+            clear = Button(gui, text='Clear', fg='black', 
+                        command=clear, height=5, width=11) 
+            clear.grid(row=5, column='1') 
+
 	
 	def closeCur(self,controller):
 		self.pack_forget()
 		controller.show_frame(PageThree)
 
+    def clear(): 
+        global expression 
+        expression = "" 
+        equation.set("")
+
 class PageThree(Frame):
+
 	def __init__(self,parent,controller):
 
 		Frame.__init__(self, parent)
